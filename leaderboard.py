8472ad7d17c79d5c65b445da4fa915f9159fc4f2
Dominik Böhler <289612+dominikb1888@users.noreply.github.com>
2022-04-20 13:43:53 +0200

    added personal time analysis

--- leaderboard.py
+++ leaderboard.py
@@ -3,14 +3,30 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import re
+from dotenv import load_dotenv   #for python-dotenv method
+load_dotenv()                    #for python-dotenv method
+
+import os 
+
 
 
 class Leaderboard():
-    def __init__(org=DB-Teaching, key="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", self):
-        self.gh = Github(key)
-        runs = repo.get_workflow_runs()
-        session, exercise = self._split_repo_name()
-        self.columns = {
+    def __init__(org=DB-Teaching, key="", self):
+        self.gh = Github(os.environ.get('GHTOKEN'))
+        self.repos = self.gh.get_organization(self.org).get_repos()
+        self.users = list(set([user.login for user in gh.get_organization("DB-Teaching").get_outside_collaborators()]))
+        self.dataframe = self._get_table()
+        self.to_csv = self._get_table.to_csv("leaderboard.csv")
+        self.heatmap_png = self._heatmap_to_png()
+
+    def __repr__():
+        return self.dataframe.to_markdown
+
+    def _get_table(self):
+        for repo in self.repos:
+            runs = repo.get_workflow_runs()
+            session, exercise = self._split_repo_name()
+            self.columns = {
                 "session": session,
                 "exercise": exercise,
                 "name": repo.name,
@@ -22,78 +38,66 @@ class Leaderboard():
                 "failed": sum([1 for w in runs if w.conclusion != "failure"]),
                 "updated_at": repo["updated_at"],
             }
-        self.repos = self.gh.get_organization(self.org).get_repos()
-        self.users = list(set([user.login for user in gh.get_organization("DB-Teaching").get_outside_collaborators()] + ["yllkaninaj"]))
-        self.dataframe = self._get_table()
-        self.to_csv = self._get_table.to_csv("leaderboard.csv")
 
+    def _check_update(self):
+        for local_repo in pd.read_csv("leaderboard.csv"):
+            if local_repo["updated"]:
+                updated_local = local_repo["updated"]
+            if repo["updated_at"] > updated_local:
+                return False
+        return True
 
-def _check_update(self):
-    for local_repo in pd.read_csv("leaderboard.csv"):
-        if local_repo["updated"]:
-            updated_local = local_repo["updated"]
-        if repo["updated_at"] > updated_local:
-            return False
-    return True
-
-
-def _split_repo_name(self):
-    for repos in self.repo:
-       splitlist = re.split("-|_", repo.name, maxsplit=2)
-       session, exercise, *rest = (
+    def _split_repo_name(self):
+        for repos in self.repo:
+            splitlist = re.split("-|_", repo.name, maxsplit=2)
+        session, exercise, *rest = (
             splitlist if len(splitlist) > 1 else (splitlist, 0, 0)
         )
-    return session, exercise
+        return session, exercise
 
-
-def prepare_table(update=False):
-
-    df = df[df["session"] != "python"]
-    df = df[df["session"] != "github"]
-
-    lb = (
-        df[["user", "session", "exercise"]]
-        .fillna(0)
-        .pivot_table(
-            columns="session",
-            values="exercise",
-            index="user",
-            aggfunc="count",
-            fill_value=0,
-            margins=True,
-            margins_name="Total",
+    @classmethod
+    def _prepare_table(update=False):
+        df = df[df["session"] != "python"]
+        df = df[df["session"] != "github"]
+        lb = (
+            df[["user", "session", "exercise"]]
+            .fillna(0)
+            .pivot_table(
+                columns="session",
+                values="exercise",
+                index="user",
+                aggfunc="count",
+                fill_value=0,
+                margins=True,
+                margins_name="Total",
+            )
+            .astype(int)
+            .sort_values(by="Total", ascending=False)
         )
-        .astype(int)
-        .sort_values(by="Total", ascending=False)
-    )
-    # .apply(lambda row: "".join(["\u258F" * int(i) for i in row if i != '']), axis=1)
-    # .to_markdown()
-    return lb, df
+        # .apply(lambda row: "".join(["\u258F" * int(i) for i in row if i != '']), axis=1)
+        # .to_markdown()
+        return lb, df
 
+    def _print_table(update=False):
+        lb, df = prepare_table(self.repos)
 
-def print_table(update=False):
-    lb, df = prepare_table(get_table())
+        ex_count = (
+            df[["session", "exercise"]]
+            .groupby("session")
+            .agg("nunique")
+            .to_dict()["exercise"]
+        )
+        lb = lb[(lb["Total"] > 1) & (lb["Total"] < 150)]
+        lb = lb[~lb.index.isin(["numbers", "list", "cipher", "search"])]
 
-    ex_count = (
-        df[["session", "exercise"]]
-        .groupby("session")
-        .agg("nunique")
-        .to_dict()["exercise"]
-    )
-    lb = lb[(lb["Total"] > 1) & (lb["Total"] < 150)]
-    lb = lb[~lb.index.isin(["numbers", "list", "cipher", "search"])]
+        rel = lb.copy(deep=True)
+        for session in ex_count.keys():
+            rel[session] = rel[session].map(lambda i: int(100 * (i / ex_count[session])))
 
-    rel = lb.copy(deep=True)
-    for session in ex_count.keys():
-        rel[session] = rel[session].map(lambda i: int(100 * (i / ex_count[session])))
+        return rel, lb
 
-    return rel, lb
-
-
-if __name__ == "__main__":
-    rel, lb = print_table(update=True)
-    sns.set(rc={"figure.figsize": (10, 8)})
-    sns.heatmap(rel.iloc[1:, 0:14], annot=lb.iloc[1:, 0:14], cmap="YlGnBu")
-    plt.savefig("leaderboard.png", dpi=400)
-
-    print(lb.to_markdown())
+    def _heatmap_to_png():
+        rel, lb = print_table(update=True)
+        sns.set(rc={"figure.figsize": (10, 8)})
+        sns.heatmap(rel.iloc[1:, 0:14], annot=lb.iloc[1:, 0:14], cmap="YlGnBu")
+        plt.savefig("leaderboard.png", dpi=400)
