f6c6072211a551851e443e1333d345cc3c2abbee
Dominik Böhler <289612+dominikb1888@users.noreply.github.com>
2022-04-26 16:15:15 +0200

    updated the data from github api

---
+++ _archive/leaderboard.fish
@@ -0,0 +1,2 @@
+cat results | sort | uniq -c | sort
+for name in (gh repo list DB-Teaching -L 500 --json name | jq '.[] | .name'); set user (string split '-' $name); set result (string join " " $user[-1]); echo $result >> results; end
