9011990c4267fc94ec83981c4b1ab034f4e90b31
ilearncloud9 <90619975+ilearncloud9@users.noreply.github.com>
2023-02-13 06:00:56 +0100

    Added date range filter (#2)
    * Load live data
    Move semester data to a separate file
    Add commit time filters
    
    * Add comments
    
    * Change input type to date
    
    * Clean up filter function

--- app.py
+++ app.py
@@ -3,26 +3,66 @@ from leaderboard import Leaderboard
 from collections import defaultdict
 import requests as rq
 import json
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 # user_repos = json.load(open("user_repos.json"))
-leaderboard = Leaderboard(org="DB-Teaching")
+semester_data = json.load(open("dumps/semester_data.json")) # Move semester_data to an importable file
+leaderboard = Leaderboard()
 user_repos = leaderboard.user_repos
 repos = leaderboard.repos
 data = leaderboard.data
 
-@app.route("/")
+
+def filtered_commits(commits, lte, gte):
+    ret = commits
+    if lte is not None:
+        ret = [commit for commit in ret if lte + timedelta(days=1) >= datetime.strptime(commit['commit']['author']['date'], "%Y-%m-%dT%H:%M:%SZ")] # filter all the commits that are after lte
+    if gte is not None:
+        ret = [commit for commit in ret if gte <= datetime.strptime(commit['commit']['author']['date'], "%Y-%m-%dT%H:%M:%SZ")] # filter all the commits that are before gte
+    return ret
+
+
+def filtered_repos(repos, lte, gte):
+    return [
+        repo for repo in [
+            {**item, 'commits': filtered_commits(item['commits'], lte, gte) } for item in repos 
+        ] if len(repo['commits']) > 0 # only keep the repos with at least one commit
+    ]
+
+
+@app.route("/", methods=["GET", "POST"])
 def heatmap():
+    if request.method == "POST":
+        start_date = request.form.get("start_date")
+        end_date = request.form.get("end_date")
+    else:
+        start_date = request.args.get("start_date")
+        end_date = request.args.get("end_date")
+    
+    filtered_items = user_repos
+
+    lte, gte = None, None
+    if end_date:
+        lte = datetime.strptime(end_date, '%Y-%m-%d')    
+    if start_date:
+        gte = datetime.strptime(start_date, '%Y-%m-%d')
+
+    filtered_items = {key: filtered_repos(values, lte, gte) for key, values in filtered_items.items() }
+    filtered_items = {key: values for key, values in filtered_items.items() if len(values) > 0 } # only keep the users with at least one commit
+    
     return render_template(
         "heatmap.html",
-        user_repos=user_repos,
+        user_repos=filtered_items,
+        start_date=start_date if start_date else "", # return filter data so jinja can auto fill them
+        end_date=end_date if end_date else "", # return filter data so jinja can auto fill them
     )
 
 @app.route("/semester/<string:semester>")
 def heatmap_semester(semester):
-    semester_data = {'22W': ["1990Flori", "AKHILB007"], '22S': ["Aleksar05", "Alexandra18636"]} # Make this importable from a file
+    # semester_data = {'22W': ["1990Flori", "AKHILB007"], '22S': ["Aleksar05", "Alexandra18636"]} # Make this importable from a file
     users = semester_data[semester]
 
     return render_template(
@@ -36,9 +76,26 @@ def heatmap_semester(semester):
 
 @app.route("/updates")
 def updates():
+    start_date = request.args.get("start_date")
+    end_date = request.args.get("end_date")
+
+    filtered_items = repos
+
+
+    lte, gte = None, None
+    if end_date:
+        lte = datetime.strptime(end_date, '%Y-%m-%d')
+    if start_date:
+        gte = datetime.strptime(start_date, '%Y-%m-%d')
+
+    filtered_items = filtered_repos(filtered_items, lte, gte)
+    
+
     return render_template(
         "list.html",
-        repos=repos,
+        repos=filtered_items,
+        start_date=start_date if start_date else "", # return filter data so jinja can auto fill them
+        end_date=end_date if end_date else "", # return filter data so jinja can auto fill them
     )
 
 
@@ -66,7 +123,7 @@ def api_users():
 
 @app.route("/api/v1/user_repos/<string:semester>")
 def api_users_semester(semester):
-    semester_data = {'22W': ["1990Flori", "AKHILB007"], '22S': ["Aleksar05", "Alexandra18636"]} # Make this importable from a file
+    # semester_data = {'22W': ["1990Flori", "AKHILB007"], '22S': ["Aleksar05", "Alexandra18636"]} # Make this importable from a file
 
     if semester in semester_data.keys():
         users = semester_data[semester]
